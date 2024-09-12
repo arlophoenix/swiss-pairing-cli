545ec081b084d32cfd3f48859a09fd5c4dce90a1
arlophoenix <arlo.phoenix@gmail.com>
2024-09-12 14:55:06 +1200

    Configure eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,43 @@
+import eslintConfigPrettier from "eslint-config-prettier";
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { 
+      globals: {
+        ...globals.node,
+        ...globals.es2021
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...tseslint.configs.strict.rules,
+      ...eslintConfigPrettier.rules,
+    },
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      ...tseslint.configs.recommendedTypeChecked.rules,
+      ...tseslint.configs.strictTypeChecked.rules,
+      // You can add or modify TypeScript-specific rules here
+    },
+  },
 ];
