c460a0dcec7679e02d5cd7d47748b09803198df0
arlophoenix <arlo.phoenix@gmail.com>
2024-09-16 14:00:13 +1200

    Update eslint config to encourage functional style

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginFunctional from "eslint-plugin-functional";
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
@@ -18,12 +19,25 @@ export default [
     },
     plugins: {
       "@typescript-eslint": tseslint.plugin,
+      "functional": eslintPluginFunctional,
     },
     rules: {
-      ...pluginJs.configs.recommended.rules,
+      ...js.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
       ...tseslint.configs.strict.rules,
       ...eslintConfigPrettier.rules,
+      // Functional style rules
+      "functional/functional-parameters": ["error", { "allowRestParameter": true, "enforceParameterCount": false }],
+      "functional/no-expression-statement": "off",
+      // TODO: enable these rules to enforce a more functional style
+      // "functional/immutable-data": "error",
+      // "functional/no-let": "error",
+      // "functional/prefer-readonly-type": "error",
+      // Prefer arrow functions
+      "prefer-arrow-callback": "error",
+      "arrow-body-style": ["error", "as-needed"],
+      // Enforce single object argument
+      "max-params": ["error", 1],
     },
   },
   {
@@ -31,13 +45,12 @@ export default [
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
-        project: ["./tsconfig.json", "./tsconfig.test.json"], // Add both configs
+        project: ["./tsconfig.json", "./tsconfig.test.json"],
       },
     },
     rules: {
       ...tseslint.configs.recommendedTypeChecked.rules,
       ...tseslint.configs.strictTypeChecked.rules,
-      // You can add or modify TypeScript-specific rules here
     },
   },
 ];
