e82a13bf799979e5c63cfe58ad4df7989ba602f9
arlophoenix <arlo.phoenix@gmail.com>
2024-09-23 21:22:02 +1200

    Update fixtures test to handle CSV and JSON files

--- test/fixtures.test.ts
+++ test/fixtures.test.ts
@@ -1,9 +1,16 @@
+import { SUPPORTED_FILE_TYPES } from '../src/constants.js';
+import { SupportedFileTypes } from '../src/types.js';
 import { execSync } from 'child_process';
 import fs from 'fs';
 import path from 'path';
 
-const runCLI = (args: string) =>
+const runCLIWithArgs = (args: string) =>
   execSync(`node dist/index.js ${args}`, {
+    encoding: 'utf-8',
+  });
+
+const runCLIWithFile = (filePath: string) =>
+  execSync(`node dist/index.js --file ${filePath}`, {
     encoding: 'utf-8',
   });
 
@@ -11,11 +18,22 @@ describe('Fixtures', () => {
   const fixturesDir = path.join(__dirname, 'fixtures');
 
   fs.readdirSync(fixturesDir).forEach((fixture) => {
-    test(`CLI Output - ${fixture}`, () => {
-      const input = fs.readFileSync(path.join(fixturesDir, fixture), 'utf-8');
-      const output = runCLI(input);
+    const ext = path.extname(fixture);
+    const fixturePath = path.join(fixturesDir, fixture);
 
-      expect(output).toMatchSnapshot();
-    });
+    if (ext === '.txt') {
+      // Load arguments directly for .txt files
+      const input = fs.readFileSync(fixturePath, 'utf-8');
+      test(`CLI Output (txt) - ${fixture}`, () => {
+        const output = runCLIWithArgs(input);
+        expect(output).toMatchSnapshot();
+      });
+    } else if (SUPPORTED_FILE_TYPES.includes(ext as SupportedFileTypes)) {
+      // Use --file for .csv and .json files
+      test(`CLI Output (file) - ${fixture}`, () => {
+        const output = runCLIWithFile(fixturePath);
+        expect(output).toMatchSnapshot();
+      });
+    }
   });
 });
