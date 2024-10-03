e006cf0d170ddc0c529cd9af35fc6078dfd83e8f
arlophoenix <arlo.phoenix@gmail.com>
2024-10-03 22:00:52 +1300

    speed up integration tests - async file access

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -4,6 +4,7 @@ import { SUPPORTED_FILE_TYPES } from '../src/constants.js';
 import { SupportedFileTypes } from '../src/types/types.js';
 import { exec } from 'child_process';
 import fs from 'fs';
+import fsPromise from 'fs/promises';
 import path from 'path';
 import util from 'util';
 
@@ -22,10 +23,10 @@ const runCLIWithFile = async (filePath: string) => {
 
 const fileCache: Record<string, string> = {};
 
-const readFileContent = (filePath: string): string => {
+const readFileContent = async (filePath: string): Promise<string> => {
   if (!fileCache[filePath]) {
     // eslint-disable-next-line functional/immutable-data
-    fileCache[filePath] = fs.readFileSync(filePath, 'utf-8');
+    fileCache[filePath] = await fsPromise.readFile(filePath, 'utf-8');
   }
   return fileCache[filePath];
 };
@@ -38,7 +39,7 @@ describe('Fixtures', () => {
     const fixturePath = path.join(fixturesDir, fixture);
 
     if (ext === '.txt') {
-      const input = readFileContent(fixturePath);
+      const input = await readFileContent(fixturePath);
       const output = await runCLIWithArgs(input);
       expect(output).toMatchSnapshot();
     } else if (SUPPORTED_FILE_TYPES.includes(ext as SupportedFileTypes)) {
@@ -46,8 +47,8 @@ describe('Fixtures', () => {
       expect(output).toMatchSnapshot();
 
       const txtPath = fixturePath.replace(ext, '.txt');
-      if (fs.existsSync(txtPath)) {
-        const inputWithArgs = readFileContent(txtPath);
+      if (fixtures.includes(path.basename(txtPath))) {
+        const inputWithArgs = await readFileContent(txtPath);
         const outputWithArgs = await runCLIWithArgs(inputWithArgs);
         expect(output).toEqual(outputWithArgs);
       }
