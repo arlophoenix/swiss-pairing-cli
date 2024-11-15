0430e857a709ae6f64e0bbfb4074647e119bba8e
arlophoenix <arlo.phoenix@gmail.com>
2024-11-15 17:32:30 +1300

    Remove unused import

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { afterEach, beforeEach, describe, expect, it, jest } from '@jest/globals';
+import { afterEach, describe, expect, it, jest } from '@jest/globals';
 
 /* eslint-disable functional/prefer-readonly-type */
 import { createCLI } from './cli/cli.js';
